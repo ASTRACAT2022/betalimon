59e11d6203672eae082a3da078b8112f13824d1a
ASTRACAT2022 <107486261+ASTRACAT2022@users.noreply.github.com>
2025-04-24 17:28:12 +0300

    Update layout.tsx

--- warpXNM/app/layout.tsx
+++ warpXNM/app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import type React from "react"
-import Script from "next/script"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import type React from "react";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Генерация конфигурации WARP",
-  description: "Генератор конфигураций для WARP"
-}
+  description: "Генератор конфигураций для WARP",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -31,27 +31,36 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
               (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-              ym(98811523, "init", {
+              ym(100530848, "init", {
                 defer: true,
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true
               });
             `,
           }}
         />
         <noscript>
           <div>
-            <img src="https://mc.yandex.ru/watch/98811523" style={{ position: "absolute", left: "-9999px" }} alt="" />
+            <img
+              src="https://mc.yandex.ru/watch/100530848"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
           </div>
         </noscript>
       </head>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
