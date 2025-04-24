495d87fde94a4ced09d90ea931207fa898e290b4
ASTRACAT2022 <107486261+ASTRACAT2022@users.noreply.github.com>
2025-04-24 17:17:02 +0300

    Delete warp-config-generator-vercel-master directory

--- warp-config-generator-vercel-master/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { WarpGenerator } from "@/components/WarpGenerator"
-
-export default function Home() {
-  return (
-    <main className="w-full min-h-screen flex items-center justify-center">
-      <WarpGenerator />
-    </main>
-  )
-}
